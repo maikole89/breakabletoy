0ec7fe6a9e1dd88dd5e9ed529a18214cada9aa57
Maikol <maikole89@gmail.com>
2018-05-10 12:10:06 -0400

    create event files

--- app/javascript/containers/EventListContainer.js
+++ app/javascript/containers/EventListContainer.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import Event from "../components/Event";
+import { Link } from 'react-router';
+
+
 
 class EventListContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { events: [] };
+    this.state = {
+      events: [],
+      errorMessage: ''
+     };
     this.getEvents = this.getEvents.bind(this);
+    this.addNewEvent = this.addNewEvent.bind(this)
 }
 
+addNewEvent(formPayload) {
+  fetch('/api/v1/reviews.json', {
+    credentials: 'same-origin',
+    method: 'post',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(formPayload)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`, error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(responseReview => {
+      if (responseReview['id'] == 'error message') {
+        this.setState({ errorMessage: responseReview['body'] })
+      } else {
+        let newReviews = this.state.reviews.concat(responseReview)
+        this.setState({
+          reviews: newReviews,
+          errorMessage: ''
+        })
+    }
+    })
+}
 
   getEvents() {
     fetch('/api/v1/events.json')
